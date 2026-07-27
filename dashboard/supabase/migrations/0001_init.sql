d90d8202a7ef13c4993fccfe4f301e1868a95f37
Daniel Feng <dxfeng08@gmail.com>
2026-07-27 10:13:39 -0400

    Added RLS and full crud permissions grants to all tables for authenticated and service_role.

--- dashboard/supabase/migrations/0001_init.sql
+++ dashboard/supabase/migrations/0001_init.sql
@@ -1,14 +1,14 @@
 CREATE TABLE repos (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL REFERENCES auth.users(id),
-    
-    repo_id BIGINT NOT NULL,
+
+    github_repo_id BIGINT NOT NULL,
     repo_name TEXT NOT NULL,
 
-    unique (repo_id)
+    UNIQUE (github_repo_id)
 );
 
-CREATE TABLE audits {
+CREATE TABLE audits (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL REFERENCES auth.users(id),
 
@@ -17,29 +17,54 @@ CREATE TABLE audits {
     audit_time TIMESTAMPTZ NOT NULL DEFAULT now(),
 
     repo_id UUID NOT NULL REFERENCES repos(id)
-}
+);
 
-CREATE TABLE detections {
+CREATE TABLE detections (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL REFERENCES auth.users(id),
 
     package TEXT NOT NULL,
     package_version TEXT NOT NULL,
     depr_status TEXT NOT NULL,
-    src TEXT NO NULL,
+    src TEXT NOT NULL,
 
     audit_id UUID NOT NULL REFERENCES audits(id)
-}
+);
 
-CREATE TABLE usages {
+CREATE TABLE usages (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL REFERENCES auth.users(id),
 
-    usage_file TEXT,
-    usage_line INT,
-    usage_code TEXT,
+    usage_file TEXT NOT NULL,
+    usage_line INT NOT NULL,
+    usage_code TEXT NOT NULL,
 
     detection_id UUID NOT NULL REFERENCES detections(id)
-}
+);
 
-CREATE POLICY "Users can only access their own repos" ON repos FOR ALL USING (auth.uid() = user_id);
+ALTER TABLE repos ENABLE ROW LEVEL SECURITY;
+ALTER TABLE audits ENABLE ROW LEVEL SECURITY;
+ALTER TABLE detections ENABLE ROW LEVEL SECURITY;
+ALTER TABLE usages ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Users can only access their own repos" ON repos
+    FOR ALL USING (auth.uid() = user_id);
+
+CREATE POLICY "Users can only read their own audits" ON audits
+    FOR ALL USING (auth.uid() = user_id);
+
+CREATE POLICY "Users can only read their own detections" ON detections
+    FOR ALL USING (auth.uid() = user_id);
+
+CREATE POLICY "Users can only read their own usages" ON usages
+    FOR ALL USING (auth.uid() = user_id);
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON repos TO authenticated;
+GRANT SELECT, INSERT, UPDATE, DELETE ON audits TO authenticated;
+GRANT SELECT, INSERT, UPDATE, DELETE ON detections TO authenticated;
+GRANT SELECT, INSERT, UPDATE, DELETE ON usages TO authenticated;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON repos TO service_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON audits TO service_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON detections TO service_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON usages TO service_role;
