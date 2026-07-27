f35f4de936229217d83a12d37432a4431f8188a5
Daniel Feng <dxfeng08@gmail.com>
2026-07-27 13:08:24 -0400

    Works on localhost to allow signin/signout using github.

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -4,6 +4,26 @@ import { supabase } from './supabaseClient'
 export default function App() {
   const [session, setSession] = useState(null)
 
+  useEffect(() => {
+    // Code fragmented from https://supabase.com/docs/guides/auth/quickstarts/react
+
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  async function signInWithGithub() {
+    await supabase.auth.signInWithOAuth({
+      provider: 'github',
+    })
+  }
+
   if (!session) {
     return (
       <>
@@ -16,7 +36,7 @@ export default function App() {
           </svg>
         </div>
         <div>
-          <button>Sign in with GitHub</button>
+          <button onClick = {signInWithGithub}>Sign in with GitHub</button>
         </div>
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
           <h3>Created by Daniel Feng</h3>
@@ -29,7 +49,12 @@ export default function App() {
   }
 
   return (
-    <div>
-    </div>
+    <>
+      <div>
+        <h1>Dashboard</h1>
+        <p>Logged in as: {session.user.email}</p>
+        <button onClick={() => supabase.auth.signOut()}>Sign Out</button>
+      </div>
+    </>
   )
 }
